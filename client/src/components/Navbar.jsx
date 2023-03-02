9eda00a80f0beb8828c24df439d5daccea160925
rkmdCodes <113243168+rkmdCodes@users.noreply.github.com>
2023-03-02 19:10:31 +0530

    Dark Mode Functionality Added

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -21,7 +21,8 @@ import {
   Toolbar,
   Menu,
   MenuItem,
-  useTheme,} from "@mui/material";
+  useTheme,
+} from "@mui/material";
 
 const Navbar = () => {
   const dispatch = useDispatch();
@@ -35,29 +36,43 @@ const Navbar = () => {
         boxShadow: "none",
       }}
     >
-
       <Toolbar sx={{ justifyContent: "space-between" }}>
-      
         <FlexBetween>
-              <IconButton onClick={()=>console.log("open/close sidebar")}>
-                   <MenuIcon/>
-              </IconButton>
-            <FlexBetween
-              backgroundColor={theme.palette.background.alt}
-              borderRadius = "9px"
-              gap = "3rem"
-              p = "0.1rem 1.5rem"
-            > 
-            <InputBase placeholder="Search....."/>
-              <IconButton>
-                  <Search/>
-              </IconButton>
-            </FlexBetween>
+          <IconButton onClick={() => console.log("open/close sidebar")}>
+            <MenuIcon />
+          </IconButton>
+
+          {/* Left side */}
+
+          <FlexBetween
+            backgroundColor={theme.palette.background.alt}
+            borderRadius="9px"
+            gap="3rem"
+            p="0.1rem 1.5rem"
+          >
+            <InputBase placeholder="Search....." />
+            <IconButton>
+              <Search />
+            </IconButton>
+          </FlexBetween>
+        </FlexBetween>
+
+        {/* Right side */}
+
+        <FlexBetween gap="1.5rem">
+           <IconButton onClick={()=>dispatch(setMode())}>
+              {theme.palette.mode === 'dark' ? (
+                <DarkModeOutlined sx ={{fontSize:"25px"}}/>
+              ):(
+                <LightModeOutlined sx = {{fontSize:"25px"}} />
+              )}
+           </IconButton>
+           <IconButton>
+              <SettingsOutlined sx = {{fontSize:"25px"}} />
+           </IconButton>
 
         </FlexBetween>
-        
 
-      
       </Toolbar>
     </AppBar>
   );
