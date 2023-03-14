ba987c0d0d955a0aee00c607749d0e258c842158
rkmdCodes <113243168+rkmdCodes@users.noreply.github.com>
2023-03-14 07:59:59 +0530

    backend implemented for geography

--- server/controller/client.js
+++ server/controller/client.js
@@ -3,6 +3,7 @@ import Product from "../models/Product.js";
 import ProductStat from "../models/ProductStat.js";
 import User from "../models/User.js";
 import Transaction from "../models/Transaction.js";
+import getCountryIso3 from "country-iso-2-to-3";
 
 export const getProducts = async (req, res) => {
   try {
@@ -39,38 +40,63 @@ export const getCustomers = async (req, res) => {
 
 export const getTransactions = async (req, res) => {
   try {
-
-    const {page = 1,pageSize = 1,sort=null,search=""} = req.query;
-    const generateSort = ()=>{
+    const { page = 1, pageSize = 1, sort = null, search = "" } = req.query;
+    const generateSort = () => {
       const sortParsed = JSON.parse(sort);
       const sortFormatted = {
-        [sortParsed.field] : sortParsed.sort ="asc"?1:-1
-      }
-     return sortFormatted;     
-    }
+        [sortParsed.field]: (sortParsed.sort = "asc" ? 1 : -1),
+      };
+      return sortFormatted;
+    };
 
     const sortFormatted = Boolean(sort) ? generateSort() : {};
-    
-    const transaction = await Transaction.find( {
 
-      $or:[
-         {cost:{$regex:new RegExp(search,"i")}},
-         {userId:{$regex:new RegExp(search,"i")}}
-    ],
+    const transaction = await Transaction.find({
+      $or: [
+        { cost: { $regex: new RegExp(search, "i") } },
+        { userId: { $regex: new RegExp(search, "i") } },
+      ],
     })
-    .sort(sortFormatted)
-    .skip(page*pageSize)
-    .limit(pageSize);
+      .sort(sortFormatted)
+      .skip(page * pageSize)
+      .limit(pageSize);
 
     const total = await Transaction.countDocuments({
       name: { $regex: search, $options: "i" },
     });
 
-
     res.status(200).json({
       transaction,
       total,
     });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+export const getGeography = async (req, res) => {
+  try {
+    const user = await User.find();
+
+    const MappedLocations = user.reduce((acc, { country }) => {
+      const countryISO3 = getCountryIso3(country);
+
+      if (!acc[countryISO3]) {
+        acc[countryISO3] = 0;
+      }
+
+      acc[countryISO3]++;
+      return acc;
+    }, {});
+   
+   const formatedData = Object.entries(MappedLocations).map(
+    ([country,count])=>{
+       return {id:country,value:count};
+    }
+
+   )    
+
+    res.status(200).json(formatedData);
   } catch (error) {
     res.status(500).json({ message: error.message });
   }
--- server/routes/client.js
+++ server/routes/client.js
@@ -2,13 +2,15 @@ import express from "express";
 import {
   getProducts,
   getCustomers ,
-  getTransactions
+  getTransactions,
+  getGeography,
 } from "../controller/client.js";
 
 const router = express.Router();
 
 router.get("/products", getProducts);
 router.get("/customers", getCustomers);
-router.get("/transactions",getTransactions)
+router.get("/transactions",getTransactions);
+router.get("/geography",getGeography);
 
 export default router;
